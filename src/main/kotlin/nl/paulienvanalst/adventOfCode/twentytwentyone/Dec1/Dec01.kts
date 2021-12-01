971fabcf7fff9af767363974962c743afaac8dc1
Paulien van Alst <coding@paulienvanalst.com>
2021-12-01 20:00:44 +0100

    Clean up and refactor properly

--- src/main/kotlin/nl/paulienvanalst/adventOfCode/twentytwentyone/Dec1/Dec01.kts
+++ src/main/kotlin/nl/paulienvanalst/adventOfCode/twentytwentyone/Dec1/Dec01.kts
@@ -7,26 +7,12 @@ val nrOfInputs = reportInput.size
 
 println("Working towards solution to part 1: reading $nrOfInputs lines of code")
 
-val counting = reportInput.foldIndexed(0) {
-    index, total, item -> if( index + 1 < nrOfInputs && reportInput[index + 1] > item) {
-        total + 1
-    } else {
-        total
-    }
-}
+val counting = reportInput.zipWithNext().count { (a, b) -> b > a }
 
 println("Solution to part 1: $counting")
 
 println("Working towards solution to part 2")
-val threeMeasurements = reportInput.subList(0, nrOfInputs - 2).mapIndexed { index, item ->
-    item + reportInput[index + 1] + reportInput[index + 2]
-}
 
-val counting2 = threeMeasurements.foldIndexed(0) {
-        index, total, item -> if(index + 1 < threeMeasurements.size && threeMeasurements[index + 1] > item) {
-    total + 1
-} else {
-    total
-}
-}
+val counting2 = reportInput.windowed(3) { it.sum() }.zipWithNext().count { (a, b) -> b > a }
+
 println("Solution to part 2: $counting2")
