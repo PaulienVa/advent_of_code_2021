e4e89a360bcc0bf84f4f276b041c724568a0fa96
Paulien van Alst <coding@paulienvanalst.com>
2021-12-02 19:41:42 +0100

    Day 2 - part 2

--- src/main/kotlin/nl/paulienvanalst/adventOfCode/twentytwentyone/Dec2/Dec02.kts
+++ src/main/kotlin/nl/paulienvanalst/adventOfCode/twentytwentyone/Dec2/Dec02.kts
@@ -7,7 +7,7 @@ val nrOfInputs = reportInput.size
 
 println("Working towards solution to part 1: reading $nrOfInputs lines of code")
 
-val finalPosition = reportInput.map { it.split(" ") }.foldRight(Pair(0,0)) {
+val finalPosition = reportInput.map { it.split(" ") }.foldRight(Pair(0L, 0L)) {
     movement, acc -> acc.addMovement(movement)
 }
 
@@ -17,17 +17,41 @@ println("Solution to part 1: $solution")
 
 println("Working towards solution to part 2")
 
-//val counting2 = reportInput.windowed(3) { it.sum() }.zipWithNext().count { (a, b) -> b > a }
+val finalPosition2 = Utils.readInput().map { it.split(" ") }.fold(Pair(Pair(0L, 0L), 0L)) {
+        acc, movement -> acc.addMovement2(movement)
+}
 
-//println("Solution to part 2: $counting2")
+val solution2 = finalPosition2.position().horizontal() * finalPosition2.position().depth()
 
-typealias Position = Pair<Int, Int>
+println("Solution to part 2: $solution2")
+
+typealias Position = Pair<Long, Long>
 fun Position.horizontal() = this.first
 fun Position.depth() = this.second
 
-fun Pair<Int, Int>.addMovement(movement: List<String>) = when(movement[0]) {
-    "forward" -> this.horizontal() + movement[1].toInt() to this.depth()
-    "down"    -> this.horizontal() to this.depth() + movement[1].toInt()
-    "up"      -> this.horizontal() to this.depth() - movement[1].toInt()
-    else -> this
+typealias PositionAndAim = Pair<Position, Long>
+fun PositionAndAim.position() = this.first
+fun PositionAndAim.aim() = this.second
+
+fun Pair<Long, Long>.addMovement(movement: List<String>) : Position {
+    val x = movement[1].toLong()
+    return when(movement[0]) {
+        "forward" -> this.horizontal() + x to this.depth()
+        "down"    -> this.horizontal() to this.depth() + x
+        "up"      -> this.horizontal() to this.depth() - x
+        else -> this
+    }
+}
+
+fun Pair<Position, Long>.addMovement2(movement: List<String>) : PositionAndAim {
+    val x = movement[1].toLong()
+    return when(movement[0]) {
+        "down" -> this.position() to this.aim() + x
+        "up" -> this.position() to this.aim() - x
+        "forward" -> {
+            val newPostion = this.position().horizontal() + x to this.position().depth() + this.aim() * x
+            newPostion to this.aim()
+        }
+        else -> this
+    }
 }
