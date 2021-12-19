ea638eb273183bd3f37373d201586e64c8ae8212
Paulien van Alst <coding@paulienvanalst.com>
2021-12-19 14:24:16 +0100

    Day 6 - part 2

--- src/main/kotlin/nl/paulienvanalst/adventOfCode/twentytwentyone/Dec6/Dec06.kts
+++ src/main/kotlin/nl/paulienvanalst/adventOfCode/twentytwentyone/Dec6/Dec06.kts
@@ -2,34 +2,46 @@
 
 import nl.paulienvanalst.adventOfCode.twentytwentyone.utils.Utils
 
-val initialState = Utils.readFileAsWhole().split(",").map{ it.trim().toInt() }
+val initialState = LongArray(9).apply {
+    Utils.readFileAsWhole().split(",").map{ it.trim().toInt() }.forEach { this[it] += 1L }
+}
 //println(initialState)
 
-val until80Days = evolution(initialState, 0).size
+val until80Days = evolution(initialState.asSequence(), 0, 80).count()
 
 println("Initial amount: ${initialState.size}")
 
 println("Solution to part 1: ${until80Days}")
+performingEvolutions(256)
+val until256Days = this.initialState.sum()
 
-println("Found")
+println("Solution to part 2: $until256Days")
 
-
-println("Solution to part 2:")
-
-println("Found")
-
-fun evolution(state: List<Int>, day: Int) : List<Int> {
-    if (day == 80) {
-        return state
+fun performingEvolutions(nrDays: Int) {
+    repeat(nrDays) {
+        initialState.evolute()
+        initialState[6] += initialState[8]
     }
-    val nr0s = state.count { it == 0 }
-    val nr8s = (1 .. nr0s).map { 8 }
-    val newState = state.map { newState(it) }
-    return evolution(newState + nr8s, day + 1)
 }
 
-fun newState(nrDays: Int) : Int {
-    return if (nrDays == 0) {
+fun LongArray.evolute() {
+    val nr0s = this[0]
+    this.copyInto(this, startIndex = 1)
+    this[this.lastIndex] = nr0s
+}
+
+fun evolution(state: Sequence<Long>, day: Int, nrDays: Int) : Sequence<Long> {
+    if (day == nrDays) {
+        return state
+    }
+    val nr0s = state.count { it == 0L }
+    val nr8s = (1 .. nr0s).map { 8L }
+    val newState = state.map { newState(it) }
+    return evolution(newState + nr8s, day + 1, nrDays)
+}
+
+fun newState(nrDays: Long) : Long {
+    return if (nrDays == 0L) {
        6
     } else {
         nrDays - 1
