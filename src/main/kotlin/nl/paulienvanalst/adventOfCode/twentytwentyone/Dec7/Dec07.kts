f5db2a91c655278fa5922aad9b40173ea432a5ec
Paulien van Alst <coding@paulienvanalst.com>
2021-12-19 15:33:42 +0100

    Day 7 part  2

--- src/main/kotlin/nl/paulienvanalst/adventOfCode/twentytwentyone/Dec7/Dec07.kts
+++ src/main/kotlin/nl/paulienvanalst/adventOfCode/twentytwentyone/Dec7/Dec07.kts
@@ -14,17 +14,39 @@ val crabFrequencies = horizontalPositionByFrequency.keys
 val lessCommonPosition = crabFrequencies.minOf { it }
 val mostCommonPostion = crabFrequencies.maxOf { it }
 
-val minimalFuel = (lessCommonPosition .. mostCommonPostion).minOf {
-    finalPosition -> horizontalPositionByFrequency.map { (position, freq) -> (position  - finalPosition).absoluteValue * freq }.sum()
+fun IntRange.minimalFuel(fuel: (Int, Int, Int) -> Int) = this.minOf {
+        finalPosition -> horizontalPositionByFrequency.map { (position, freq) -> fuel(position, finalPosition, freq) }.sum()
 }
 
-println("Solution to part 1: $minimalFuel")
+fun fuelPart1(position: Int, finalPosition: Int, freq: Int) =
+    (position - finalPosition).absoluteValue * freq
 
-println("Found")
+fun fuelPart2(position: Int, finalPosition: Int, freq: Int) = (minOf(position, finalPosition) until maxOf(
+    position,
+    finalPosition
+)).sumOf { it - minOf(position, finalPosition) + 1 } *  freq
 
+val minimalFuel1 = (lessCommonPosition .. mostCommonPostion).minimalFuel { position, finalPosition, freq ->
+    fuelPart1(
+        position,
+        finalPosition,
+        freq
+    )
+}
 
-println("Solution to part 2:")
+println("Solution to part 1: $minimalFuel1")
 
-println("Found")
+val testFuel2 =  fuelPart2(16, 5, 1)
 
+println("Test: ${(minOf(5, 16) .. maxOf(5, 16)).map {it - minOf(5,16)} + 1 }")
+println("Test: $testFuel2")
 
+val minimalFuel2 = (lessCommonPosition .. mostCommonPostion).minimalFuel { position, finalPosition, freq ->
+    fuelPart2(
+        position,
+        finalPosition,
+        freq
+    )
+}
+
+println("Solution to part 2: $minimalFuel2")
